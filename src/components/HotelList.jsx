37d4d98053b81e8d151995723a617677b508e3dc
Daniel160407 <abulashvilidani@hotmail.com>
2024-04-21 19:15:01 +0400

    Initial commit

---
+++ src/components/HotelList.jsx
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import '../style/HotelList.scss';
+
+function HotelList(){
+    const [hotelData, setHotelData] = useState({ hotels: [] });
+    const [roomData, setRoomData] = useState({ rooms: [] });
+    const [showRooms, setShowRooms] = useState(false);
+    const [selectedHotel, setSelectedHotel] = useState('');
+    const [selectedRoom, setSelectedRoom] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/Booking/hotels')
+        .then(response => {
+            setHotelData(response.data);
+        });
+    }, []);
+
+    function showHotelRooms(hotelName){
+        axios.get(`http://localhost:8080/Booking/rooms?hotelName=${hotelName}`)
+        .then(response => {
+            setRoomData(response.data);
+        });
+        setShowRooms(true);
+        setSelectedHotel(hotelName);
+    }
+
+    function chooseRoom(hotelName, roomName){
+        axios.put(`http://localhost:8080/Booking/rooms`,{
+            name: roomName,
+            hotelName: hotelName,
+            roomState: 'RESERVED'
+        });
+    }
+
+    function handleSubmit(){
+        chooseRoom(selectedHotel, selectedRoom);
+        setShowRooms(false);
+    }
+
+    return(
+        <div id="hotelList">
+            <div id="header">
+                <h1>Booking</h1>
+            </div>
+            {hotelData.hotels.length > 0 && (
+                <ul>
+                    {hotelData.hotels.map(hotel => (
+                        <li key={hotel.name} onClick={() => showHotelRooms(hotel.name)}>{hotel.name}</li>
+                    ))}
+                </ul>
+            )}
+
+            {showRooms && (
+                <div id="hotelData">
+                    <div id="hotelInfo">
+                        <div id="hotelName">
+                            <h1>{selectedHotel}</h1>
+                        </div>
+                    {roomData.rooms.length > 0 && (
+                        <select id="roomList" value={""} onChange={(e) => setSelectedRoom(e.target.value)}>
+                            <option disabled value="">Select a room</option>
+                            {roomData.rooms.map(room => (
+                                <option key={room.name}>{room.name}</option>
+                            ))}
+                        </select>
+                    )}
+                    <div id="submit">
+                        <button type="submit" onClick={handleSubmit} disabled={!selectedRoom}>Submit</button>
+                    </div>
+                    </div>
+                </div>
+                
+            )}
+        </div>
+    );
+}
+
+export default HotelList;
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HotelList from './components/HotelList.jsx'
+import './style/Main.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <HotelList />
+);
